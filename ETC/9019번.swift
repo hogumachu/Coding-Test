d2cfce7c747b275744887a5958db5ea2b724a857
hogumachu <castle_jun@daum.net>
2021-05-11 22:21:16 +0900

    [백준] 9019 DSLR

--- ETC/9019번.swift
+++ ETC/9019번.swift
@@ -1,70 +1,73 @@
-// 시간초과 !!!! 다시 풀기
-// L과 R를 계산할 때 시간 초과가 나는 듯 ?
-
 // 출처 : 백준 DSLR
 // https://www.acmicpc.net/problem/9019
 // 풀이 : hogumachu
+// removeFirst를 여러 번 사용하니 시간 초과가 뜸
+// 추가적으로 String으로 결과를 계속 보내는 것 보다
+// Int로 해서 보내는 게 더 빠름
 
 import Foundation
 
 func solution() {
     let t = Int(readLine()!)!
-    var queue: [(Int, String, Int)] = []
-    var counting: [Int] = Array(repeating: 1000000, count: 10000)
-    let newCounting = counting
-    let newQueue: [(Int, String, Int)] = []
-    var printResult = ""
-    func DSLR(_ start: Int, _ end: Int, _ result: String, _ count: Int) -> Void {
-        let D = start * 2 > 9999 ? start * 2 % 10000 : start * 2
-        let S = start == 0 ? 9999 : start-1
-
-        if D == end {
-            printResult = result+"D"
-            return
-        } else if counting[D] > count + 1{
-            counting[D] = count + 1
-            queue.append((D, result+"D", count + 1))
-        }
-
-        if S == end {
-            printResult = result+"S"
-            return
-        } else if counting[S] > count + 1 {
-            counting[S] = count + 1
-            queue.append((S, result+"S", count + 1))
-        }
-
-        let L = start % 1000 * 10 + start / 1000
-        let R = start % 10 * 1000 + start / 10
-
-        if L == end {
-            printResult = result+"L"
-            return
-        } else if counting[L] > count + 1{
-            counting[L] = count + 1
-            queue.append((L, result+"L", count + 1))
-        }
-
-        if R == end {
-            printResult = result+"R"
-            return
-        } else if counting[R] > count + 1{
-            counting[R] = count + 1
-            queue.append((R, result+"R", count + 1))
-        }
-    }
-
     for _ in 0..<t {
+        var visited: [Bool] = Array(repeating: false, count: 10000)
+        var queue: [(Int, Int)] = []
+        var printResult = ""
+        var index = 0
         let values = readLine()!.split(separator: " ").map{Int(String($0))!}
-        queue.append((values[0], "", values[1]))
-        while queue.isEmpty == false && printResult == "" {
-            let first = queue.removeFirst()
-            DSLR(first.0, values[1], first.1, first.1.count)
+        queue.append((values[0], 0))
+        visited[values[0]] = true
+        while true {
+            if printResult != "" {
+                break
+            }
+            let select = queue[index]
+            let start = select.0, end = values[1], result = select.1
+            index += 1
+            if start == end {
+                for i in String(result) {
+                    switch i {
+                    case "1":
+                        printResult += "D"
+                    case "2":
+                        printResult += "S"
+                    case "3":
+                        printResult += "L"
+                    case "4":
+                        printResult += "R"
+                    default:
+                        break
+                    }
+                }
+                break
+            } else {
+                let D = start * 2 % 10000
+                let S = start == 0 ? 9999 : start-1
+                let L = (start % 1000) * 10 + start / 1000
+                let R = (start % 10) * 1000 + start / 10
+
+                if visited[D] == false {
+                    visited[D] = true
+                    queue.append((D, result*10+1))
+                }
+
+                if visited[S] == false {
+                    visited[S] = true
+                    queue.append((S, result*10+2))
+                }
+
+                if visited[L] == false {
+                    visited[L] = true
+                    queue.append((L, result*10+3))
+                }
+
+                if visited[R] == false {
+                    visited[R] = true
+                    queue.append((R, result*10+4))
+                }
+            }
         }
         print(printResult)
-        printResult = ""
-        queue = newQueue
-        counting = newCounting
     }
 }
 
