1edeb52436adf53b364547adc9152fc301d5f734
hogumachu <castle_jun@daum.net>
2021-05-15 00:01:22 +0900

    [백준] 11053 가장 긴 증가하는 부분 수열

---
+++ ETC/11053번.swift
@@ -0,0 +1,20 @@
+// 출처 : 백준 가장 긴 증가하는 부분 수열
+// https://www.acmicpc.net/problem/11053
+// 풀이 : hogumachu
+
+import Foundation
+
+func solution() {
+    _ = readLine()
+    let values: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+    var table: [Int] = Array(repeating: 1, count: values.count)
+
+    for i in 0..<values.count {
+        for j in 0..<i {
+            values[i] > values[j] ? (table[i] = max(table[i], table[j] + 1)) : nil
+        }
+    }
+    print(table.max()!)
+}
+
+solution()
