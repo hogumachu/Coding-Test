74dd37139ee8f1e265a7ebcb4935dd57f9d4ecbb
hogumachu <castle_jun@daum.net>
2021-08-16 19:20:51 +0900

    [프로그래머스] 퍼즐 조각 채우기

--- ETC/퍼즐 조각 채우기.swift
+++ ETC/퍼즐 조각 채우기.swift
@@ -1,68 +1,32 @@
 // 출처 : 프로그래머스 위클리 챌린지 3주차
 // https://programmers.co.kr/learn/courses/30/lessons/84021
 // 풀이 : hogumachu
-// 중복된 코드가 너무 많음
-// 너무 대충 짠듯 ... ?
 
 import Foundation
 
 func solution(_ game_board:[[Int]], _ table:[[Int]]) -> Int {
     var result = 0
-    let bp = boardPath(game_board)
-    var tp = tablePath(table)
+    let bp = makePath(game_board, 0, 1)
+    var tp = makePath(table, 1, 0)
     var bpMatched: [Int] = []
     var tpMatched: [Int] = []
 
-    for i in 0..<bp.count {
-        for j in 0..<tp.count {
-            if bp[i].count == tp[j].count && !bpMatched.contains(i) && !tpMatched.contains(j) {
-                if canMatch(bp[i], tp[j]) {
-                    result += bp[i].count
-                    bpMatched.append(i)
-                    tpMatched.append(j)
+    for k in 0..<4 {
+        for i in 0..<bp.count {
+            for j in 0..<tp.count {
+                if bp[i].count == tp[j].count && !bpMatched.contains(i) && !tpMatched.contains(j) {
+                    if canMatch(bp[i], tp[j]) {
+                        result += bp[i].count
+                        bpMatched.append(i)
+                        tpMatched.append(j)
+                    }
                 }
             }
         }
-    }
-
-    tp = rotate(tp)
-
-    for i in 0..<bp.count {
-        for j in 0..<tp.count {
-            if bp[i].count == tp[j].count && !bpMatched.contains(i) && !tpMatched.contains(j) {
-                if canMatch(bp[i], tp[j]) {
-                    result += bp[i].count
-                    bpMatched.append(i)
-                    tpMatched.append(j)
-                }
-            }
+        if k == 3 {
+            break
         }
-    }
-    tp = rotate(tp)
-
-    for i in 0..<bp.count {
-        for j in 0..<tp.count {
-            if bp[i].count == tp[j].count && !bpMatched.contains(i) && !tpMatched.contains(j) {
-                if canMatch(bp[i], tp[j]) {
-                    result += bp[i].count
-                    bpMatched.append(i)
-                    tpMatched.append(j)
-                }
-            }
-        }
-    }
-    tp = rotate(tp)
-
-    for i in 0..<bp.count {
-        for j in 0..<tp.count {
-            if bp[i].count == tp[j].count && !bpMatched.contains(i) && !tpMatched.contains(j) {
-                if canMatch(bp[i], tp[j]) {
-                    result += bp[i].count
-                    bpMatched.append(i)
-                    tpMatched.append(j)
-                }
-            }
-        }
+        tp = rotate(tp)
     }
 
     return result
@@ -83,54 +47,7 @@ func canMatch(_ board: [(Int, Int)], _ table: [(Int, Int)]) -> Bool {
     return true
 }
 
-func boardPath(_ board: [[Int]]) -> [[(Int, Int)]] {
-    var newBoard = board
-    var path: [[(Int, Int)]] = []
-    let n = newBoard.count
-
-    func addPath(_ x: Int, _ y: Int) {
-        var queue: [(Int, Int)] = []
-        var queueIndex = 0
-
-        let moveX = [-1, 0, 1, 0]
-        let moveY = [0, 1, 0, -1]
-
-        queue.append((x, y))
-        newBoard[x][y] = 1
-
-        while queueIndex < queue.count {
-            let select = queue[queueIndex]
-            queueIndex += 1
-
-            for i in 0..<moveX.count {
-                if select.0 + moveX[i] >= 0 && select.0 + moveX[i] < n && select.1 + moveY[i] >= 0 && select.1 + moveY[i] < n, newBoard[select.0 + moveX[i]][select.1 + moveY[i]] == 0 {
-                    newBoard[select.0 + moveX[i]][select.1 + moveY[i]] = 1
-                    queue.append((select.0 + moveX[i], select.1 + moveY[i]))
-                }
-            }
-        }
-        queue.sort(by: {
-             if $0.0 == $1.0 {
-                return $0.1 < $1.1
-            } else {
-                return $0.0 < $1.0
-            }
-        })
-        path.append(queue)
-    }
-
-    for i in 0..<n {
-        for j in 0..<n {
-            if newBoard[i][j] == 0 {
-                addPath(i, j)
-            }
-        }
-    }
-
-    return path
-}
-
-func tablePath(_ table: [[Int]]) -> [[(Int, Int)]] {
+func makePath(_ table: [[Int]], _ a: Int, _ b: Int) -> [[(Int, Int)]] {
     var newTable = table
     var path: [[(Int, Int)]] = []
     let n = newTable.count
@@ -143,15 +60,15 @@ func tablePath(_ table: [[Int]]) -> [[(Int, Int)]] {
         let moveY = [0, 1, 0, -1]
 
         queue.append((x, y))
-        newTable[x][y] = 0
+        newTable[x][y] = b
 
         while queueIndex < queue.count {
             let select = queue[queueIndex]
             queueIndex += 1
 
             for i in 0..<moveX.count {
-                if select.0 + moveX[i] >= 0 && select.0 + moveX[i] < n && select.1 + moveY[i] >= 0 && select.1 + moveY[i] < n, newTable[select.0 + moveX[i]][select.1 + moveY[i]] == 1 {
-                    newTable[select.0 + moveX[i]][select.1 + moveY[i]] = 0
+                if select.0 + moveX[i] >= 0 && select.0 + moveX[i] < n && select.1 + moveY[i] >= 0 && select.1 + moveY[i] < n, newTable[select.0 + moveX[i]][select.1 + moveY[i]] == a {
+                    newTable[select.0 + moveX[i]][select.1 + moveY[i]] = b
                     queue.append((select.0 + moveX[i], select.1 + moveY[i]))
                 }
             }
@@ -168,12 +85,11 @@ func tablePath(_ table: [[Int]]) -> [[(Int, Int)]] {
 
     for i in 0..<n {
         for j in 0..<n {
-            if newTable[i][j] == 1 {
+            if newTable[i][j] == a {
                 addPath(i, j)
             }
         }
     }
-
     return path
 }
 
