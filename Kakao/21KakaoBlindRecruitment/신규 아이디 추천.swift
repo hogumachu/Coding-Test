835abd19a959fad4a2f475e57c91ed2e3e73a639
hogumachu <castle_jun@daum.net>
2021-08-20 08:27:32 +0900

    [프로그래머스] 신규 아이디 추천

--- Kakao/21KakaoBlindRecruitment/신규 아이디 추천.swift
+++ Kakao/21KakaoBlindRecruitment/신규 아이디 추천.swift
@@ -2,56 +2,66 @@
 // https://programmers.co.kr/learn/courses/30/lessons/72410
 // 풀이 : hogumachu
 
-
-
 import Foundation
 
-func solution(_ new_id:String) -> String {
-    var id: [Character] = []
-    var beforeCharacter: Character = Character(" ")
-    for c in new_id {
-        if c.asciiValue! >= 65 && c.asciiValue! <= 90 {
-            id.append(Character(UnicodeScalar(c.asciiValue!+32)))
-            beforeCharacter = Character(UnicodeScalar(c.asciiValue!+32))
-        } else if c.asciiValue! >= 97 && c.asciiValue! <= 122 {
-            id.append(c)
-            beforeCharacter = c
-        } else if c == Character("-") || c == Character("_") || c == Character(".") {
-            if !(beforeCharacter == Character(".") && c == Character(".")) {
-                id.append(c)
-                beforeCharacter = c
+func solution(_ new_id: String) -> String {
+    // String 을 lowercased 를 이용하여 소문자로 만들어줌
+    let lower = new_id.lowercased()
+
+    var arr: [String] = []
+    var before = 0
+
+    lower.forEach {
+        // asciiValue 를 이용하여 숫자, 알파벳, 뺴기, 밑줄, 마침표에 해당한 값들만 배열에 추가하였음
+
+        let asc = Int($0.asciiValue!)
+        switch asc {
+        case 45, 46, 48...57, 95, 97...122:
+        // 추가할 때 이전 값이 마침표이고 현재 값이 마침표라면 마침표 2번 이상 연속된 부분이므로 추가하지 않음
+            if before == 46 && asc == 46 {
+                break
+            } else {
+                arr.append("\($0)")
             }
-        } else if c == Character("0") || c == Character("1") || c == Character("2") || c == Character("3") || c == Character("4") || c == Character("5") || c == Character("6") || c == Character("7") || c == Character("8") || c == Character("9") {
-            id.append(c)
-            beforeCharacter = c
+            before = asc
+        default:
+            break
         }
     }
-    while id.first == Character(".") {
-        id.removeFirst()
+
+    // 전부 다 추가한 후 배열의 첫번째가 마침표가 아닐 때 까지 제거
+    while arr.first == "." {
+        arr.removeFirst()
     }
-    while id.last == Character(".") {
-        id.removeLast()
+
+    // 배열의 마지막이 마침표가 아닐 때 까지 제거
+    while arr.last == "." {
+        arr.removeLast()
     }
-    if id.count >= 16 {
-        for _ in 15..<id.count {
-            id.removeLast()
-        }
+
+    // 만약 배열이 비어있다면 "a" 를 추가함
+    if arr.isEmpty {
+        arr.append("a")
     }
-    while id.last == Character(".") {
-        id.removeLast()
+
+    // 배열의 크기가 15를 넘어가면 배열의 크기를 15로 줄여줌
+    while arr.count > 15 {
+        arr.removeLast()
     }
-    if id.count == 0 {
-        id.append(Character("a"))
+
+    // 배열의 마지막이 마침표가 아닐 때 까지 제거
+    while arr.last == "." {
+        arr.removeLast()
     }
-    if id.count <= 2 && id.count != 0 {
-        let addWord = id.last!
-        while id.count != 3 {
-            id.append(addWord)
-        }
+
+    // 만약 배열의 크기가 3보다 작으면 배열의 마지막 값을 배열의 크기가 3이 될 때 까지 추가하였음 (강제 언래핑을 사용해도 되는데 그냥 "a" 를 추가함)
+    while arr.count < 3 {
+        arr.append(arr.last ?? "a")
     }
-    var result = ""
-    for i in id {
-        result += "\(i)"
-    }
+
+    // 마지막으로 배열을 String 으로 변경하여 값을 제출함. result 라 안하고 그냥 그대로 return 해도 상관 없음.
+    
+    let result = arr.reduce("", +)
+
     return result
 }
