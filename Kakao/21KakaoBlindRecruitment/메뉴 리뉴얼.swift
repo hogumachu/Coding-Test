1cc36857adf3195eec3c5a1d8f97ef2276e7bc29
hogumachu <castle_jun@daum.net>
2021-09-03 13:57:31 +0900

    [프로그래머스] 메뉴 리뉴얼

--- Kakao/21KakaoBlindRecruitment/메뉴 리뉴얼.swift
+++ Kakao/21KakaoBlindRecruitment/메뉴 리뉴얼.swift
@@ -1,63 +1,88 @@
 // 출처 : 프로그래머스 2021 KAKAO BLIND RECRUITMENT 메뉴 리뉴얼
 // https://programmers.co.kr/learn/courses/30/lessons/72411
 // 풀이 : hogumachu
+// orders 에 있는 String 에 대해 2개 이상 선택하여 값을 더하고 그에 대한 filter 로 답을 해결하는 문제
 
 import Foundation
 
-func solution(_ orders: [String], _ course: [Int]) -> [String] {
-    // key - value 저장을 위한 dictionary 와 여러 배열을 생성함.
+func solution(_ orders:[String], _ course:[Int]) -> [String] {
+    // 값의 개수를 셀 Dictionary 생성
     var dict: [String: Int] = [:]
-    var ord: [[String]] = Array(repeating: [], count: orders.count + 1)
-    var getCourse: [Int] = Array(repeating: 0, count: course.last! + 1)
+
+    // String 으로 된 orders 를 배열로 변경함
+    var arrOrders: [[Character]] = Array(repeating: [], count: orders.count)
+
+    // 코스 수의 최대값을 저장할 배열
+    var maxCount: [Int] = Array(repeating: 0, count: course.last! + 1)
+
+    // 결과
     var result: [String] = []
 
-    // dictionary 에 order 를 저장하는 함수
-    // order가 몇 번 발생했는 지를 파악하는 함수
-    func appendDict(_ arr: [Character], _ count: Int, _ index: Int, _ counting: Int, _ str: String) {
-        if counting == count {
-            if dict[str] != nil {
-                dict[str]! += 1
-            } else {
-                dict[str] = 1
-            }
-        } else {
-            if index >= arr.count {
+    // orders 를 배열로 만들어서 arrOrders 에 저장해줌
+    // course 는 정렬되어 있지만, orders 에는 정렬되어 있다는 얘기가 없음 -> 값을 오름차순으로 정렬해야함
+    for i in 0..<orders.count {
+        let arrOrder = orders[i].map { $0 }
+        arrOrders[i] = arrOrder.sorted()
+    }
+
+    // 핵심 함수
+    func visit(_ order: [Character], _ arr: [Character] ,_ index: Int) {
+        // 코스 2개 이상 선택했을 때
+        if arr.count > 1 {
+            // 만약 코스 수가 course 배열에 있는 마지막 값보다 크게 되면 굳이 구할 필요 없으므로 return
+            if arr.count > course.last! {
                 return
             }
-            for i in index..<arr.count {
-                appendDict(arr, count, i + 1, counting + 1, str + "\(arr[i])")
+
+            // 코스를 String 으로 변경하고
+            let str = String(arr)
+
+            // Dictionary 에 추가함
+            if dict[str] == nil {
+                dict[str] = 1
+            } else {
+                dict[str]! += 1
+                // 만약 현재 코스 수가 최대 값이면 갱신
+                maxCount[str.count] = max(maxCount[str.count], dict[str]!)
             }
         }
-    }
 
-    // order 를 정렬하여 배열로 만들고 위에 있는 appendDict 를 실행함
-    for order in orders {
-        let sortedOrder = order.sorted()
-        for count in course {
-            appendDict(sortedOrder, count, 0, 0, "")
+        // 핵심 함수 반복
+        for i in index..<order.count {
+            visit(order, arr + [order[i]], i + 1)
         }
     }
 
-    // ord 배열에 2번 이상 order 가 되었다면 저장함
-    for element in dict {
-        if element.value > 1 {
-            ord[element.value].append(element.key)
+    // 핵심 함수 반복
+    for order in arrOrders {
+        visit(order, [], 0)
+    }
+
+    // Dictionary 를 문제 조건에 맞춰 필터링함
+    dict.filter {
+        // 코스의 주문 수는 1번 이상만 가능 (1번 일때는 X, 0번은 저장되지 않으므로 쓸 이유 없음)
+        if $0.value == 1 {
+            return false
+
+            // 만약 현재 코스 주문 수가 최대값일 때 저장함
+        } else if maxCount[$0.key.count] == $0.value {
+            return true
+            
+            // 그렇지 않으면 리턴
+        } else {
+            return false
+        }
+    }
+    // 결과가 key 에 대해 오름차순으로 정렬되어 있으므로 정렬함
+    .sorted {
+        $0.key < $1.key
+
+        // forEach 로 하여 course 에서 얘기한 정보만 result 에 append
+    }.forEach {
+        if course.contains($0.key.count) {
+            result.append($0.key)
         }
     }
 
-    // 저장된 ord 의 배열 뒤부터 시작하여 getCourse 에 저장된 값보다 작거나 같을 시에 result 에 order 를 저장함
-    for i in stride(from: ord.count - 1, to: 1, by: -1) {
-        for value in ord[i] {
-            // value.count 는 course 가 어떤 것인지 (2코스인지 3코스인지) 알 수 있고
-            // getCourse[value.count] 는 해당 코스에 저장된 값 (그 코스를 최대 몇 번 시켰는지) 에 대해 알 수 있음
-            // 만약 그 코스의 횟수와 동일하게 시키거나 많이 시켰다면 그 코스의 최대값 (getCourse[value.count]) 는 i가 되고 result 에 그 order 를 append 함 
-            if getCourse[value.count] <= i {
-                getCourse[value.count] = i
-                result.append(value)
-            }
-        }
-    }
-
-    // 오름차순으로 정렬하여 리턴
-    return result.sorted()
+    return result
 }
