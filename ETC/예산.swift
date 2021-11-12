cd2b6fbb8e72c250590c15ff56120d7b4628d112
hogumachu <castle_jun@daum.net>
2021-11-12 14:19:53 +0900

    [프로그래머스] 예산

--- ETC/예산.swift
+++ ETC/예산.swift
@@ -2,12 +2,11 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12916
 // 풀이 : hogumachu
 
-
 import Foundation
 
 func solution(_ d:[Int], _ budget:Int) -> Int {
     var result = 0
-    var count = d.sorted().reduce(0){
+    _ = d.sorted().reduce(0){
         if $0 + $1 <= budget {
             result += 1
             return $0 + $1
