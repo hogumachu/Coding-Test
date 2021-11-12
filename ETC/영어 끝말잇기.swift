4175bfecc1fbcbd7cbb35d5e4b17654df4f2bef9
hogumachu <castle_jun@daum.net>
2021-11-12 15:23:23 +0900

    [프로그래머스] 영어 끝말잇기

--- ETC/영어 끝말잇기.swift
+++ ETC/영어 끝말잇기.swift
@@ -6,33 +6,19 @@ import Foundation
 
 func solution(_ n:Int, _ words:[String]) -> [Int] {
     var result = [0, 0]
-    var now = 1
-    var repeating = 1
-    var visited: [String] = []
-    for word in words {
-        if visited.contains(word) {
-            result[0] = now
-            result[1] = repeating
+    var dictionary: [String: Bool] = [:]
+    var before = words[0].first!
+
+    for i in 0..<words.count {
+        if dictionary[words[i]] != nil || before != words[i].first! {
+            result[0] = (i % n) + 1
+            result[1] = (i / n) + 1
             break
         } else {
-            if visited.count == 0 {
-                visited.append(word)
-            } else {
-                if visited.last!.last! != word.first! {
-                    result[0] = now
-                    result[1] = repeating
-                    break
-                } else {
-                    visited.append(word)
-                }
-            }
-        }
-
-        now += 1
-        if now > n {
-            now = 1
-            repeating += 1
+            before = words[i].last!
+            dictionary[words[i]] = true
         }
     }
+
     return result
 }
